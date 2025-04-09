9977e98f3789f6dc202500e5ec1b124d16f4b13c
Sumit <sumit.kr.singh14@gmail.com>
2025-04-09 15:49:58 +0530

    Add app/src/main/java/com/example/genericapi/navigation/Navigation.kt 1744193997357

---
+++ app/src/main/java/com/example/genericapi/navigation/Navigation.kt
@@ -0,0 +1,22 @@
+package com.example.genericapi.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.genericapi.ui.screens.HomeScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+        // Will add detail screen route later
+    }
+}
